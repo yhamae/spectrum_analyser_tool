bc6a46ff51cf59f5a1bdbdc6e75e54814dbaf308
Yuki Hamae <k7319438@kadai.jp>
2019-04-17 14:57:33 +0900

    edit

---
+++ get_data.py
@@ -0,0 +1,22 @@
+
+def get_data(channel, freq, T, filename, mode)
+
+	mode = d
+	with open(filename) as f
+		line = f.readlines()
+
+	for i in len(line):
+		if mode == "d":
+			print(line[i])
+
+		tmp = line[i].split()
+
+		if len(tmp) > 2:
+			continue
+
+		channel = tmp1[0]
+		freq = tmp1[1]
+		T = tmp1[2]
+
+		if tmp1[0].isalpha():
+			continue
--- maser_search.py
+++ maser_search.py
@@ -5,12 +5,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def find(x, y, peak, snr, width)
-	MADFM = madfm(y)
+    
 
-	y2 = []
+	# for i in range(0, iter - 1)
+
+	    MADFM = madfm(y)
+	    x1 = []
+	    y2 = []
+	    tmp2 = []
+	    tmp3 = []
+	    for j in range(0, len(x) - width - 1, width + 1):
+	        tmp = 0
+	        x2.append(j)
+	        for k in range(j, width + j):
+                tmp += y[k]
+	        y2.append(tmp)
+
+
+	    for j in range(1, len(y2) - 1):
+	    	if y2[j] - y2[j - 1] > 0 && y[j + 1] - y[j] < 0:
+	    		tmp2.append(j)
+
+	    for channel_no in tmp2:
+	        if y[channel_no] >= snr * MADFM:
+	            tmp3.append(channel_no)
+	    if tmp3 == NULL:
+	    	print("##### Can not find peak channel!#####")
+
+	    for j in tmp3:
+	    	tmp4 = []
+	    	for k in range(j * width, j * width + width):
+	    		tmp3.append(y[k])
+	    	max(tmp4)
+	    	del tmp4
 
 
 
-	for channel in len(x)
-		if y[channel] >= snr * MADFM
-			peak.append(x[channel])
+	    del x2, y2
+
