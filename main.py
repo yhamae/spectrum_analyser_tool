f013fe1921b756bb274a02fb7ee5d3833c302a4a
Yuki Hamae <k7319438@kadai.jp>
2019-05-22 12:20:25 +0900

    edit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import get_data
 import maser_search
+
+
 
 
 mode = "d"
@@ -8,6 +9,6 @@ channel = []
 freq = []
 T = []
 
-result01 = get_data.data(channel, freq, T, "i18286_H2O_181223.txt", mode)
+result01 = read_file.data(channel, freq, T, "i18286_H2O_181223.txt", mode)
 print(result01)
 
