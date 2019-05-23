fe60019e49219d7af21bd9096292bf21313ec5db
Yuki Hamae <k7319438@kadai.jp>
2019-05-23 17:12:02 +0900

    def class

--- get_data.py
+++ get_data.py
@@ -1,17 +1,26 @@
 import numpy as np
 
-class data:
-	def get_data(channel, freq, T, filename, mode):
+class NRODataReduction:
+	def __init__(self):
+		self.channel = 0
+		self.freq = 0
+		self.T = 0
+		self.filename = ""
+		self.mode = 0
+
+
+
+	def get_data(self):
 
 		# mode = "d"
 		try:
-			with open(filename) as f:
+			with open(self.filename) as f:
 				line = f.readlines()
 
 			for data in line:
 				tmp = data.split()
 
-				if mode == "d":
+				if self.mode == "d":
 					print("Debag_mode >    data : ", end="")
 					print(data, end = "")
 					print("Debag_mode >    len(tmp) = ", end="")
@@ -21,7 +30,7 @@ class data:
 
 
 				if len(tmp) > 1 and tmp[0].isnumeric():
-					if mode == "d":
+					if self.mode == "d":
 						print("Debag_mode >    !!!found data!!!")
 						print("Debag_mode >    channel = ", end="")
 						print(tmp[0])
@@ -29,15 +38,15 @@ class data:
 						print(tmp[1])
 						print("Debag_mode >    T = ", end="")
 						print(tmp[2])
-					channel.append(tmp[0])
-					freq.append(tmp[1])
-					T.append(tmp[2])
+					self.channel.append(tmp[0])
+					self.freq.append(tmp[1])
+					self.T.append(tmp[2])
 
 						
 				# del tmp
-			if mode == "d":
-				for i in range(0, len(channel)):
-					print(channel[i] + "    " + freq[i] +  "    " + T[i])
+			if self.mode == "d":
+				for i in range(0, len(self.channel)):
+					print(self.channel[i] + "    " + self.freq[i] +  "    " + self.T[i])
 				
 
 		except AttributeError as e:
@@ -46,7 +55,7 @@ class data:
 			return -1
 		except EOFError as e:
 			print(e)
-			print("Filename: " + filename)
+			print("Filename: " + self.filename)
 		except KeyboardInterrupt as e:
 			print(e)
 			print("fource quit!")
