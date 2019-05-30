3ebacf115a78b2b8712ad1253af29ae3c6e421a3
Yuki Hamae <k7319438@kadai.jp>
2019-05-30 12:36:34 +0900

    Create plot.py

---
+++ plot.py
@@ -0,0 +1,14 @@
+import traceback
+import numpy as np
+import matplotlib
+
+class MyPlot:
+    def __init__(self):
+        self.x1 = 0
+        self.x2 = 0
+        self.y1 = 0
+        self.y2 = 0
+        self.rms = 0
+        self.fname = ""
+    def plot(self):
+        
