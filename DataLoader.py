3627260a737bb8e5d61e81576539a8cf87b05336
Yuki Hamae <k7319438@kadai.jp>
2019-07-13 20:50:56 +0900

    ✨ : Add new function
    ある値以下のデータは読み込まない機能を実装

--- DataLoader.py
+++ DataLoader.py
@@ -13,6 +13,7 @@ class GetSpectrum:
         self.mode = 0
         self.date = ""
         self.MJD = 0
+        self.min = -50
 
 
 
@@ -30,7 +31,7 @@ class GetSpectrum:
                 if 'LSCRT9(3) = file create day' in tmp:
                     day = int(tmp.split("=")[2].strip())
 
-                if len(tmp) > 1 and tmp[0].isnumeric():
+                if len(tmp) > 1 and tmp[0].isnumeric() and tmp[2] >= self.min:
                     if 'd' in self.mode:
                         print("------------------------------")
                         print('>>>     {0:>5}  {1:>10.9}  {1:>10.9}'.format(tmp[0], tmp[1], tmp[2]))
