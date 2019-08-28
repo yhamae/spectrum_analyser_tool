100c6a34eab05e00edc295093a409ca3d92280b4
Yuki Hamae <k7319438@kadai.jp>
2019-08-28 12:58:09 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -14,25 +14,25 @@ Python3 TrackFreq.py i18251 H2O /Users/yhamae/OneDrive/astro/FLASHING/peak/ /Use
 Python3 TrackFreq.py w43 H2O /Users/yhamae/OneDrive/astro/FLASHING/peak/ /Users/yhamae/Desktop/FLASHING/w43_H20.txt
 Python3 TrackFreq.py oh16 H2O /Users/yhamae/OneDrive/astro/FLASHING/peak/ /Users/yhamae/Desktop/FLASHING/oh16_H20.txt
 
-echo -e "\033[0;32mHSiOv3\033[0;39m"
+echo -e "\033[0;32mSiOv3\033[0;39m"
 Python3 TrackFreq.py i18286 SiOv3 /Users/yhamae/OneDrive/astro/FLASHING/peak/ /Users/yhamae/Desktop/FLASHING/i18286_SiOv3.txt
 Python3 TrackFreq.py i18251 SiOv3 /Users/yhamae/OneDrive/astro/FLASHING/peak/ /Users/yhamae/Desktop/FLASHING/i18251_SiOv3.txt
 Python3 TrackFreq.py w43 SiOv3 /Users/yhamae/OneDrive/astro/FLASHING/peak/ /Users/yhamae/Desktop/FLASHING/w43_SiOv3.txt
 Python3 TrackFreq.py oh16 SiOv3 /Users/yhamae/OneDrive/astro/FLASHING/peak/ /Users/yhamae/Desktop/FLASHING/oh16_SiOv3.txt
 
-echo -e "\033[0;32mHSiOv2\033[0;39m"
+echo -e "\033[0;32mSiOv2\033[0;39m"
 Python3 TrackFreq.py i18286 SiOv2 /Users/yhamae/OneDrive/astro/FLASHING/peak/ /Users/yhamae/Desktop/FLASHING/i18286_SiOv2.txt
 Python3 TrackFreq.py i18251 SiOv2 /Users/yhamae/OneDrive/astro/FLASHING/peak/ /Users/yhamae/Desktop/FLASHING/i18251_SiOv2.txt
 Python3 TrackFreq.py w43 SiOv2 /Users/yhamae/OneDrive/astro/FLASHING/peak/ /Users/yhamae/Desktop/FLASHING/w43_SiOv2.txt
 Python3 TrackFreq.py oh16 SiOv2 /Users/yhamae/OneDrive/astro/FLASHING/peak/ /Users/yhamae/Desktop/FLASHING/oh16_SiOv2.txt
 
-echo -e "\033[0;32mHSiOv1\033[0;39m"
+echo -e "\033[0;32mSiOv1\033[0;39m"
 Python3 TrackFreq.py i18286 SiOv1 /Users/yhamae/OneDrive/astro/FLASHING/peak/ /Users/yhamae/Desktop/FLASHING/i18286_SiOv1.txt
 Python3 TrackFreq.py i18251 SiOv1 /Users/yhamae/OneDrive/astro/FLASHING/peak/ /Users/yhamae/Desktop/FLASHING/i18251_SiOv1.txt
 Python3 TrackFreq.py w43 SiOv1 /Users/yhamae/OneDrive/astro/FLASHING/peak/ /Users/yhamae/Desktop/FLASHING/w43_SiOv1.txt
 Python3 TrackFreq.py oh16 SiOv1 /Users/yhamae/OneDrive/astro/FLASHING/peak/ /Users/yhamae/Desktop/FLASHING/oh16_SiOv1.txt
 
-echo -e "\033[0;32mHSiOv0\033[0;39m"
+echo -e "\033[0;32mSiOv0\033[0;39m"
 Python3 TrackFreq.py i18286 SiOv0 /Users/yhamae/OneDrive/astro/FLASHING/peak/ /Users/yhamae/Desktop/FLASHING/i18286_SiOv0.txt
 Python3 TrackFreq.py i18251 SiOv0 /Users/yhamae/OneDrive/astro/FLASHING/peak/ /Users/yhamae/Desktop/FLASHING/i18251_SiOv0.txt
 Python3 TrackFreq.py w43 SiOv0 /Users/yhamae/OneDrive/astro/FLASHING/peak/ /Users/yhamae/Desktop/FLASHING/w43_SiOv0.txt
